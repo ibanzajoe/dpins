dccbf173ec1d35e40d8884e7c915c657eec48d94
ibanzajoe <ibanzajoe@gmail.com>
2017-05-08 11:44:08 -0700

    working on layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,215 @@
 <template>
   <div>
+    <div class="top-bar dark">
+      <!-- Nested Container Starts -->
+      <div class="container clearfix text-center-xs">
+        <p class="pull-left-lg pull-left-md pull-left-sm">
+          Welcome to Fin Advisor website, we have expertise in managing your finances!!!
+        </p>
+        <p class="pull-right-lg pull-right-md pull-right-sm text-right-lg text-right-md text-right-sm">
+          <i class="fa fa-clock-o"></i>&nbsp;
+          Monday - Friday : 8:00 AM to 7:00 PM
+        </p>
+      </div>
+      <!-- Nested Container Ends -->
+    </div>
+    <!-- Top Bar Ends -->
+    <!-- Header Starts -->
+    <header class="main-header one">
+      <!-- Nested Container Starts -->
+      <div class="container text-center-xs">
+        <!-- Nested Row Starts -->
+        <div class="row">
+          <!-- Logo Starts -->
+          <div class="col-lg-3 col-sm-4 col-xs-12">
+            <img src="images/logo.png" alt="Fin Adviser" class="img-responsive img-center-xs logo">
+          </div>
+          <!-- Logo Ends -->
+          <!-- Address Starts -->
+          <div class="col-sm-3 col-xs-12 hidden-md hidden-sm hidden-xs col-lg-offset-1">
+            <div class="clearfix">
+              <i class="fa fa-map-marker pull-left-lg pull-left-md pull-left-sm circle"></i>
+              <h6 class="text-spl-color text-medium">345 Begum Bazar,</h6>
+              <p class="text-light"><em>Near Afzaljung, Hyd - 035</em></p>
+            </div>
+          </div>
+          <!-- Address Ends -->
+          <!-- Call Us Starts -->
+          <div class="col-lg-2 col-sm-3 col-xs-12 hidden-xs">
+            <div class="clearfix text-right-md text-right-sm">
+              <i class="fa fa-phone pull-left-lg pull-right-md pull-right-sm circle"></i>
+              <h6 class="text-spl-color text-medium">Call Us On</h6>
+              <p class="text-light"><em>1-800-123-4567</em></p>
+            </div>
+          </div>
+          <!-- Call Us Ends -->
+          <!-- Mail Us Starts -->
+          <div class="col-lg-3 col-sm-5 col-xs-12 hidden-xs">
+            <div class="clearfix text-right-md text-right-sm">
+              <i class="fa fa-envelope-o pull-left-lg pull-right-md pull-right-sm circle"></i>
+              <h6 class="text-spl-color text-medium">Mail Us @</h6>
+              <p class="text-light"><em><a href="#">support@finadvisordomain.com</a></em></p>
+            </div>
+          </div>
+          <!-- Mail Us Ends -->
+        </div>
+        <!-- Nested Row Ends -->
+      </div>
+      <!-- Nested Container Ends -->
+    </header>
+
+
     <nuxt/>
+
+
+    <section class="footer-top-1">
+      <!-- Nested Container Starts -->
+      <div class="container text-center-xs">
+        <!-- Nested Row Starts -->
+        <div class="row">
+          <!-- Left Col Starts -->
+          <div class="col-md-6 col-xs-12">
+            <!-- Contact Info Fin Advisor Starts -->
+            <h4 class="section-heading-1 text-center-xs">Contact Info <span>Fin Advisor</span></h4>
+            <p>
+              <strong>There are many variations of passages of Lorem Ipsum</strong> available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
+            </p>
+            <div class="row">
+              <!-- Phone Col Starts -->
+              <div class="col-sm-4 col-xs-12">
+                <div class="foot-col-1">
+                  <h5><i class="fa fa-phone"></i> Phone</h5>
+                  <ul class="list-unstyled list-phone-no">
+                    <li>(123) 45678910</li>
+                    <li>001 998 956 1234</li>
+                  </ul>
+                </div>
+              </div>
+              <!-- Phone Col Ends -->
+              <!-- Email Starts -->
+              <div class="col-sm-4 col-xs-12">
+                <div class="foot-col-1">
+                  <h5><i class="fa fa-envelope"></i> Email</h5>
+                  <ul class="list-unstyled list-email">
+                    <li><a href="#">support@demosite.com</a></li>
+                    <li><a href="#">info@ demosite.com</a></li>
+                  </ul>
+                </div>
+              </div>
+              <!-- Email Ends -->
+              <!-- Address Starts -->
+              <div class="col-sm-4 col-xs-12">
+                <div class="foot-col-1">
+                  <h5><i class="fa fa-map-marker"></i> Address</h5>
+                  <ul class="list-unstyled list-address">
+                    <li><strong>#781, Shah Ali Banda, </strong></li>
+                    <li>Chairminar,  Telangana</li>
+                  </ul>
+                </div>
+              </div>
+              <!-- Address Ends -->
+            </div>
+            <!-- Contact Info Fin Advisor Ends -->
+            <!-- Popular Tags Starts -->
+            <h4 class="section-heading-1 text-center-xs">Popular <span>Tags</span></h4>
+            <ul class="list-unstyled list-inline list-post-tags animation">
+              <li><a href="#">Income</a></li>
+              <li><a href="#">Business</a></li>
+              <li><a href="#">Planning</a></li>
+              <li><a href="#">Aduting</a></li>
+              <li><a href="#">Finance</a></li>
+              <li><a href="#">Financial Advisor</a></li>
+              <li><a href="#">Assessment</a></li>
+            </ul>
+            <!-- Popular Tags Ends -->
+          </div>
+          <!-- Left Col Ends -->
+          <!-- Right Col Starts -->
+          <div class="col-md-6 col-xs-12">
+            <!-- Schedule an Appointment Starts -->
+            <h4 class="section-heading-1 text-center-xs">Schedule an <span>Appointment</span></h4>
+            <form action="#" class="appointment-form">
+              <!-- Nested Row Starts -->
+              <div class="row">
+                <!-- First Name Starts -->
+                <div class="col-sm-6 col-xs-12">
+                  <input type="text" class="form-control flat animation" placeholder="First Name">
+                </div>
+                <!-- First Name Ends -->
+                <!-- Last Name Starts -->
+                <div class="col-sm-6 col-xs-12">
+                  <input type="text" class="form-control flat animation" placeholder="Last Name">
+                </div>
+                <!-- Last Name Ends -->
+                <!-- Email Starts -->
+                <div class="col-sm-6 col-xs-12">
+                  <input type="email" class="form-control flat animation" placeholder="E-mail">
+                </div>
+                <!-- Email Ends -->
+                <!-- Type of Service Starts -->
+                <div class="col-sm-6 col-xs-12">
+                  <select class="form-control flat animation">
+                    <option>Type of Services</option>
+                    <option value="Services1">Services 1</option>
+                    <option value="Services2">Services 2</option>
+                    <option value="Services3">Services 3</option>
+                  </select>
+                </div>
+                <!-- Type of Service Ends -->
+                <!-- Phone No Starts -->
+                <div class="col-sm-6 col-xs-12">
+                  <input type="tel" class="form-control flat animation" placeholder="Phone No">
+                </div>
+                <!-- Phone No Ends -->
+                <!-- Location Starts -->
+                <div class="col-sm-6 col-xs-12">
+                  <select class="form-control flat animation">
+                    <option>Locations</option>
+                    <option value="Location1">Location 1</option>
+                    <option value="Location2">Location 2</option>
+                    <option value="Location3">Location 3</option>
+                  </select>
+                </div>
+                <!-- Location Ends -->
+                <!-- Message Starts -->
+                <div class="col-xs-12">
+                  <textarea rows="5" class="form-control flat animation" placeholder="Message"></textarea>
+                </div>
+                <!-- Message Ends -->
+                <!-- Submit Button Starts -->
+                <div class="col-xs-12">
+                  <button type="submit" class="btn btn-big btn-secondary animation flat">Book Appointment</button>
+                </div>
+                <!-- Submit Button Ends -->
+              </div>
+              <!-- Nested Row Ends -->
+            </form>
+            <!-- Schedule an Appointment Ends -->
+          </div>
+          <!-- Right Col Ends -->
+        </div>
+        <!-- Nested Row Ends -->
+      </div>
+      <!-- Nested Container Ends -->
+    </section>
+    <!-- Footer Top Section Ends -->
+    <!-- Copyright Starts -->
+    <footer class="copyright-1">
+      <!-- Nested Container Starts -->
+      <div class="container text-center text-lite-color">
+        <ul class="list-unstyled list-inline">
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+        </ul>
+        <em class="text-light">Fin Advisor &copy; 2017. All Rights Reserved. Designed By <a href="#">Sainath Chillapuram</a></em>
+      </div>
+      <!-- Nested Container Ends -->
+    </footer>
+    <!-- Copyright Ends -->
+
   </div>
 </template>
 
@@ -50,4 +259,8 @@ html
   color: #fff;
   background-color: #35495e;
 }
+
+  .container {
+    min-height: 100%;
+  }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'starter',
     meta: [
@@ -10,29 +10,37 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'css/bootstrap.min.css'},
+      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' },
+      { rel: 'stylesheet', href: 'font-awesome/css/font-awesome.min.css' },
+      { rel: 'stylesheet', href: 'js/plugins/owl-carousel/owl.carousel.css'},
+      { rel: 'stylesheet', href: 'js/plugins/owl-carousel/owl.theme.css' },
+      { rel: 'stylesheet', href: 'js/plugins/magnific-popup/magnific-popup.css' },
+      { rel: 'stylesheet', href: 'css/style.css' },
+      { rel: 'stylesheet', href: 'css/responsive.css' },
+      { rel: 'apple-touch-icon-precomosed', sizes: '144x144', href: 'images/fav-144.png'},
+      { rel: 'apple-touch-icon-precomposed', sizes: '114x114', href: 'images/fav-72.png'},
+      { rel: 'apple-touch-icon-precomposed', sizes: '72x72', href: 'images/fav-57.png'},
+      { rel: 'apple-touch-icon-precomposed', href: 'images/fav-57.png' },
+      { rel: 'shortcut icon', href: 'images/fav.png' }
+    ],
+    script: [
+      { src: 'https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js' },
+      { src: 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js' }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#3B8070' },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+     ** Run ESLINT on save
+     */
   }
 }
