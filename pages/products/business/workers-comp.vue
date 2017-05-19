415966ad198ef9a10868f58c74ec46eba01c6995
dennislee <ibanzajoe@gmail.com>
2017-05-19 00:22:13 -0700

    added life-insurance and workers compensation page to individual and business insurance

---
+++ pages/products/business/workers-comp.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <section class="main-banner text-center-xs">
+      <!-- Nested Container Starts -->
+      <div class="container text-lite-color">
+        <h2 class="text-medium">Workers' Compensation</h2>
+      </div>
+      <!-- Nested Container Ends -->
+    </section>
+    <!-- Main Banner Ends -->
+    <!-- Breadcrumb Starts -->
+    <div class="container text-center-xs">
+      <ol class="breadcrumb flat">
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li>Business</li>
+        <li class="active">Workers' Compensation</li>
+      </ol>
+    </div>
+    <!-- Breadcrumb Ends -->
+    <!-- Main Container Starts -->
+    <div class="main-container container">
+      <!-- Nested Row Starts -->
+      <div class="row text-center-xs">
+        <!-- Image Media Starts -->
+        <div class="col-sm-5 col-xs-12">
+          <img src="/images/about/about-img3.jpg" alt="About Image" class="img-responsive img-style-1 img-center-xs">
+          <p class="hidden visible-xs"><br></p>
+        </div>
+        <!-- Image Media Ends -->
+        <!-- Content Starts -->
+        <div class="col-sm-7 col-xs-12">
+          <p class="lead">
+            <strong>Why do you need workers' compensation insurance?</strong>
+          </p>
+          <p>
+            Workers compensation insurance provides coverage of medical care and other compensation of benefits to employees who are insured in
+            the course of employment in exchange for mandatory relinquishment of the employee's right to sue their employer for tort of negligence.
+          </p>
+          <p>
+            There is a tradeoff between the assured, limited coverage and lack of recourse outside the worker compensation system called
+            "the compensation bargain".  While schemes differ between jurisdictions, provisions can be made for weekly payments in place of wages,
+            compensation for economic loss, reimbursement and payment of medical expenses, and benefits payable to the dependents of workers killed
+            during employment.
+          </p>
+        </div>
+        <!-- Content Ends -->
+      </div>
+      <!-- Nested Row Ends -->
+      <!-- Spacer Starts -->
+      <div class="spacer-medium"></div>
+      <!-- Spacer Ends -->
+      <!-- Nested Row Starts -->
+      <div class="row text-center-xs">
+        <!-- Content Starts -->
+        <div class="col-sm-7 col-xs-12">
+          <p class="lead">
+            <strong>How does it work?</strong>
+          </p>
+          <p>
+            California's workers' compensation act provides an example of a comprehensive state compensation program.
+            It is applicable to most employers.  The statue limits the liability of the employer and fellow employees.
+            California also requires employers to obtain insurance to cover potential workers' compensation claims, and
+            sets up a fund claims that employers have illegally failed to insure against.
+          </p>
+        </div>
+        <!-- Content Ends -->
+        <!-- Image Media Starts -->
+        <div class="col-sm-5 col-xs-12">
+          <p class="hidden visible-xs"><br></p>
+          <img src="/images/about/about-img4.jpg" alt="About Image" class="img-responsive img-style-1 img-center-xs">
+        </div>
+        <!-- Image Media Ends -->
+      </div>
+      <!-- Nested Row Ends -->
+      <!-- Spacer Starts -->
+      <div class="spacer"></div>
+      <!-- Spacer Ends -->
+      <!-- What We Offer Starts -->
+      <h4 class="main-heading-1 text-spl-color text-uppercase text-center-xs">What We Offer</h4>
+      <ul class="list-unstyled row list-style-1">
+        <!-- Offer List #1 Starts -->
+        <li class="col-md-4 col-sm-6 col-xs-12 clearfix">
+          <i class="fa fa-cogs pull-left circle"></i>
+          <h6 class="text-bold">Find Nearest Branch</h6>
+          <p class="text-grey-color">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          </p>
+        </li>
+        <!-- Offer List #1 Ends -->
+        <!-- Offer List #2 Starts -->
+        <li class="col-md-4 col-sm-6 col-xs-12 clearfix">
+          <i class="fa fa-thumbs-up pull-left circle"></i>
+          <h6 class="text-bold">Join Our Company</h6>
+          <p class="text-grey-color">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          </p>
+        </li>
+        <!-- Offer List #2 Ends -->
+        <!-- Offer List #3 Starts -->
+        <li class="col-md-4 col-sm-6 col-xs-12 clearfix">
+          <i class="fa fa-bullhorn pull-left circle"></i>
+          <h6 class="text-bold">365 Days Support</h6>
+          <p class="text-grey-color">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          </p>
+        </li>
+        <!-- Offer List #3 Ends -->
+        <!-- Offer List #4 Starts -->
+        <li class="col-md-4 col-sm-6 col-xs-12 clearfix">
+          <i class="fa fa-puzzle-piece pull-left circle"></i>
+          <h6 class="text-bold">Latest Technologies</h6>
+          <p class="text-grey-color">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          </p>
+        </li>
+        <!-- Offer List #4 Ends -->
+        <!-- Offer List #5 Starts -->
+        <li class="col-md-4 col-sm-6 col-xs-12 clearfix">
+          <i class="fa fa-book pull-left circle"></i>
+          <h6 class="text-bold">Well Document Presents</h6>
+          <p class="text-grey-color">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          </p>
+        </li>
+        <!-- Offer List #5 Ends -->
+        <!-- Offer List #6 Starts -->
+        <li class="col-md-4 col-sm-6 col-xs-12 clearfix">
+          <i class="fa fa-users pull-left circle"></i>
+          <h6 class="text-bold">Team Work</h6>
+          <p class="text-grey-color">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          </p>
+        </li>
+        <!-- Offer List #6 Ends -->
+      </ul>
+      <!-- What We Offer Ends -->
+      <!-- Spacer Starts -->
+      <div class="spacer-medium"></div>
+      <!-- Spacer Ends -->
+    </div>
+  </div>
+</template>
