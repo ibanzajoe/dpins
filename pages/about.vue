f995654ad380781df78923b6d68dd89880c48143
James Lee <uptownhr@gmail.com>
2017-05-21 22:53:26 -0700

    about us image samples added

--- pages/about.vue
+++ pages/about.vue
@@ -22,7 +22,7 @@
       <div class="row text-center-xs">
         <!-- Image Media Starts -->
         <div class="col-sm-5 col-xs-12">
-          <img src="/images/about/about-img3.jpg" alt="About Image" class="img-responsive img-style-1 img-center-xs">
+          <img src="http://media.istockphoto.com/photos/insurance-picture-id481429165" alt="About Image" class="img-responsive img-style-1 img-center-xs">
           <p class="hidden visible-xs"><br></p>
         </div>
         <!-- Image Media Ends -->
@@ -70,7 +70,7 @@
         <!-- Image Media Starts -->
         <div class="col-sm-5 col-xs-12">
           <p class="hidden visible-xs"><br></p>
-          <img src="/images/about/about-img4.jpg" alt="About Image" class="img-responsive img-style-1 img-center-xs">
+          <img src="http://media.istockphoto.com/photos/empowered-to-lead-an-empire-picture-id608628094?s=2048x2048" alt="About Image" class="img-responsive img-style-1 img-center-xs">
         </div>
         <!-- Image Media Ends -->
       </div>
