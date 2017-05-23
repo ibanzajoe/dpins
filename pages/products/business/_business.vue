088c409e9bd8fed8cf8b04aba7b73bc234557fc4
dennislee <ibanzajoe@gmail.com>
2017-05-23 01:49:34 -0700

    added in side_image as part of content of the temporary data content

--- pages/products/business/_business.vue
+++ pages/products/business/_business.vue
@@ -25,7 +25,7 @@
           <div class="row text-center-xs">
             <!-- Image Media Starts -->
             <div class="col-sm-5 col-xs-12">
-              <img src="/images/about/about-img3.jpg" alt="About Image" class="img-responsive img-style-1 img-center-xs">
+              <img :src="content.side_image" alt="About Image" class="img-responsive img-style-1 img-center-xs">
               <p class="hidden visible-xs"><br></p>
             </div>
             <!-- Image Media Ends -->
@@ -101,7 +101,7 @@
             <!-- Image Media Starts -->
             <div class="col-sm-5 col-xs-12">
               <p class="hidden visible-xs"><br></p>
-              <img src="/images/about/about-img4.jpg" alt="About Image" class="img-responsive img-style-1 img-center-xs">
+              <img :src="content.side_image" alt="About Image" class="img-responsive img-style-1 img-center-xs">
             </div>
             <!-- Image Media Ends -->
           </div>
@@ -146,7 +146,8 @@
                 subheading: [
                   "When running a business, you HAVE to consider your liabilities. Even if you don't have a big, prosperous, business, liability insurance is essential and can be a crucial part of protecting your business and financial health.",
                   "Liability insurance covers employees and customer injuries that may occur at your place of business. Even in cases where someone you are not associated with causes someone else injury at your place of business, your business may still be held liable for damages. Liability insurance can cover you from such claims of personal injury as well as any other claims involving any type of abuse and sexual harassment."
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 heading: "What are the different types of business insurances (commonly used)?",
@@ -158,7 +159,8 @@
                   "Manufacturers and Contractors Liabilities (M&C)",
                   "Owners, Landlords, and Tenants Liability Insurance Policy (OL&T)",
                   "Physicians, Surgeons, and Dentists Professional Liability"
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               }
             ]
           },
@@ -172,14 +174,16 @@
                 subheading: [
                   "Workers compensation insurance provides coverage of medical care and other compensation of benefits to employees who are insured in the course of employment in exchange for mandatory relinquishment of the employee's right to sue their employer for tort of negligence.",
                   'There is a tradeoff between the assured, limited coverage and lack of recourse outside the worker compensation system called "the compensation bargain". While schemes differ between jurisdictions, provisions can be made for weekly payments in place of wages, compensation for economic loss, reimbursement and payment of medical expenses, and benefits payable to the dependents of workers killed during employment.'
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 heading: "How does it work?",
                 pre_subheading: '',
                 subheading: [
                   "California's workers' compensation act provides an example of a comprehensive state compensation program. It is applicable to most employers. The statue limits the liability of the employer and fellow employees. California also requires employers to obtain insurance to cover potential workers' compensation claims, and sets up a fund claims that employers have illegally failed to insure against."
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               }
             ]
           },
@@ -193,7 +197,8 @@
                 subheading: [
                   "Errors and Omission Insurances protect your company from claims made by clients who hold you responsible for errors or some other failure regarding the work performed as promised in your contract.",
                   "Error and Omissions insurance coverage extends to both W2 employees as well as 1099 subcontractors and can be worldwide in scope."
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 heading: "When do I need Errors and Omissions Insurance?",
@@ -201,7 +206,8 @@
                 subheading: [
                   "Error and Omissions Insurance is generally recommended to be at the foundation of every company's insurance portfolio. It is usually wise to purchase the coverage prior to product launch or when you start servicing customers.",
                   "Coverage includes legal defense costs, no matter how baseless the allegations. With Error and Omission insurance, you will be reimbursed for any resulting judgement against you including the court cost, up to the coverage limits of your policy."
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               },
               {
                 heading: "Why do I need Errors & Omissions Insurance?",
@@ -209,7 +215,8 @@
                 subheading: [
                   "Mistakes Happen. Every company messes up at some point. You can't be everywhere and sometimes you can't personally handle every job. Errors and Omissions coverage insures not only your mistakes, but the mistakes of your employees and independent contractors you hire as well.",
                   "Most Importantly: Errors and Omissions insurance can save you from extreme embarrassment, a lost client, and a bad reputation."
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               }
             ]
           },
@@ -223,7 +230,8 @@
                 subheading: [
                   "D&O Liability provides coverage for lawsuits that arise out of wrongful acts, or errors and omission-type claims. Examples of such claims under this policy would be: employment-related claims like negligent hiring practices or wrongful termination, acts beyond the authority of the league or chapter, wrongful suspension of players, coaches, or officials, selection and eligibility disputes, matters involving changes to, interpretation of, or implementation of rules, sexual harassment, and gender, racial, or ethnic discrimination.",
                   "If the officers of your chapter, league, or association is sued for these type of incidents, coverage for such claim would fall under the D&O policy."
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 heading: "",
@@ -234,7 +242,8 @@
                 subheading: [
                   "",
                   ""
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               },
               {
                 heading: "Who is covered by D&O insurance?",
@@ -243,7 +252,8 @@
                   "The D&O policy protects the individual directors and officers of the chapter or league.  " +
                   "It also covers the chapter or league entity itself, as well as members of the chapter/league " +
                   "while acting in an offical capacity, such as coaches, managers, or volunteers"
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 heading: "Who should purchase the coverage?",
@@ -252,7 +262,8 @@
                   "US Lacrosse highly recommends that all chapters purchase D&O liability insurance to protect their " +
                   "board and cover their operations.  The coverage is also available to lacrosse officials' associations " +
                   "and to leagues who are comprised of US Lacrosse members."
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               }
             ]
           },
@@ -267,7 +278,8 @@
                   "Builder's Risk Coverage protects your materials and supplies during the construction " +
                   "process.  It also covers your property in transit as well as at a temporary location where it will " +
                   "be stored"
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 heading: "How does it work?",
@@ -280,7 +292,8 @@
                   " valuable papers",
                   " the first $100,000 of debris removal",
                   " site preparation, including re-excavating a foundation or re-grading land."
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               }
             ]
           },
@@ -298,7 +311,8 @@
                   "employer reported to the carrier during the coverage period.",
                   "EPLI policies vary from carrier to carrier.  most EPLI policies provide 'duty to defend' coverage, " +
                   "requiring the carrier to defend against claims brought under the policy."
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 heading: "",
@@ -314,7 +328,8 @@
                   "recommended.  Some hammer clause allow the carrier to force the case into arbitration, " +
                   "mediation, or other alternative dispute resolution mechanisms.",
                   "EPLI policies also share some of these features or characteristics:"
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               },
               {
                 heading: "1. Covered Insurance",
@@ -326,7 +341,8 @@
                   "independent contractors from coverage.  Employers should try to obtain the broadest coverage " +
                   "possible so that part-time, temporary, leased and seasonal employees and independent " +
                   "contractors also are covered. "
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 heading: "2. Claims Covered",
@@ -341,7 +357,8 @@
                   "request to toll the statute of limitations. Depending on the size of the " +
                   "company and its financial resources, a company may wish to opt for an EPLI " +
                   "policy that is expansive in the kinds of claims covered."
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               },
               {
                 heading: "3. Person Bringing Covered Claim",
@@ -355,7 +372,8 @@
                   "EEOC 'on behalf of' employees. Employers should examine their " +
                   "workforces and determine which type of policy will best meet " +
                   "their needs."
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 heading: "4. Wrongful Acts Covered",
@@ -368,7 +386,8 @@
                   "invasion of privacy, defamation and intentional infliction of emotional " +
                   "distress. Employers should compare EPLI policies for the most comprehensive " +
                   "policy in terms of the wrongful acts covered."
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               },
               {
                 heading: "5. Practices or Acts Excluded",
@@ -390,7 +409,8 @@
                   "profit or advantage, purposeful violation of law, wrongful acts committed " +
                   "with actual knowledge of their wrongful nature or with intent to cause " +
                   "damage and other egregious conduct."
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 pre_heading: "Things to consider when buying an EPLI Policy?",
@@ -398,21 +418,24 @@
                 pre_subheading: '',
                 subheading: [
                   "Evaluate EPLI carriers based on their experience and financial strength. A low premium will not be the bargain it seemed if the carrier leaves the marketplace. It is better to go with an established carrier that is committed to the EPLI product for the long term. Bottom line: You should get considerable background information about any carrier and the underwriter writing the policy whenever possible. If the carrier is new to EPLI, it might be a good idea to consider another."
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               },
               {
                 heading: "2. Buyer Beware",
                 pre_subheading: '',
                 subheading: [
                   "An investigation into and analysis of your EPLI needs is absolutely necessary before any purchase. Do not expect underwriters to see every eventuality for you. Make sure that you can live with the claims definition and exclusions in the policy. Seek advice early if you are unsure what your needs are."
-                ]
+                ],
+                side_image: "/images/about/about-img3.jpg"
               },
               {
                 heading: "3. Be able to choose your own lawyer and settle when you want.",
                 pre_subheading: '',
                 subheading: [
                   "Choosing your own lawyer may or may not be important to you. If it is important, make sure that you address it with the carrier up front. If your attorney is experienced in employment law, the insurer should readily accommodate your request. If not, you may be forced to use panel counsel. Before accepting the insurer's panel counsel, you should find out whether the panel attorneys limit their practice to employment law, which is a very specialized area. You also will want to maintain some control over the settlement of claims because settling too quickly can cause complaints to multiply in the workplace."
-                ]
+                ],
+                side_image: "/images/about/about-img4.jpg"
               }
             ]
           }
