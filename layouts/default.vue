5574fcef504be4dfa2ccee12134862cb9482a514
James Lee <uptownhr@gmail.com>
2017-06-12 22:38:40 -0700

    increased logo size

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,16 +5,16 @@
       <!-- Nested Container Starts -->
       <div class="container text-center-xs">
         <!-- Nested Row Starts -->
-        <div class="row">
+        <div class="row flex">
           <!-- Logo Starts -->
-          <div class="col-lg-3 col-sm-4 col-xs-12">
+          <div class="col-lg-4 col-sm-4 col-xs-12">
             <nuxt-link to="/">
-              <img src="/images/logo/dp logo.png" alt="DP Insurance" class="img-responsive img-center-xs logo">
+              <img src="/images/logo/dp logo.png" alt="DP Insurance" class="img-responsive img-center-xs logo" style="">
             </nuxt-link>
           </div>
           <!-- Logo Ends -->
           <!-- Address Starts -->
-          <div class="col-sm-4 col-xs-12 hidden-md hidden-sm hidden-xs">
+          <div class="col-sm-3 col-xs-12 hidden-md hidden-sm hidden-xs">
             <div class="clearfix">
               <div class="pull-right-lg">
                 <h6 class="text-spl-color text-medium">1130 S San Gabriel Blvd</h6>
@@ -288,51 +288,51 @@
 </script>
 
 <style>
-html
-{
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-*, *:before, *:after
-{
-  box-sizing: border-box;
-  margin: 0;
-}
-.button--green
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-.button--green:hover
-{
-  color: #fff;
-  background-color: #3b8070;
-}
-.button--grey
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-.button--grey:hover
-{
-  color: #fff;
-  background-color: #35495e;
-}
+  html
+  {
+    font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
+  *, *:before, *:after
+  {
+    box-sizing: border-box;
+    margin: 0;
+  }
+  .button--green
+  {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
+  }
+  .button--green:hover
+  {
+    color: #fff;
+    background-color: #3b8070;
+  }
+  .button--grey
+  {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #35495e;
+    color: #35495e;
+    text-decoration: none;
+    padding: 10px 30px;
+    margin-left: 15px;
+  }
+  .button--grey:hover
+  {
+    color: #fff;
+    background-color: #35495e;
+  }
 
   .container {
     min-height: 100%;
@@ -341,4 +341,14 @@ html
   .button_border {
     border-bottom: 1px solid #3e3e3e;
   }
+
+  .main-header {
+    padding-top: 20px;
+  }
+
+  .row.flex {
+    display: flex;
+    align-items: center;
+  }
+
 </style>
